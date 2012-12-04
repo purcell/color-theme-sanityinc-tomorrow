5e970a86b022ab4669d40ae46edf93829ea10242
Steve Purcell <steve@sanityinc.com>
2012-12-04 11:39:31 +0000

    Add diredp-mode-line-marked

--- color-theme-sanityinc-tomorrow.el
+++ color-theme-sanityinc-tomorrow.el
@@ -299,6 +299,7 @@ names to which it refers are bound."
      (diredp-ignored-file-name ((,class (:foreground ,comment))))
      (diredp-link-priv ((,class (:background nil :foreground ,purple))))
      (diredp-mode-line-flagged ((,class (:foreground ,red))))
+     (diredp-mode-line-marked ((,class (:foreground ,green))))
      (diredp-no-priv ((,class (:background nil))))
      (diredp-number ((,class (:foreground ,yellow))))
      (diredp-other-priv ((,class (:background nil :foreground ,purple))))

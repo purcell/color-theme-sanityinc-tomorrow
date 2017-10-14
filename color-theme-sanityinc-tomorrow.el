acd7887256a77c07006fb45ebab67f1cd68df2f6
Steve Purcell <steve@sanityinc.com>
2017-10-15 10:30:28 +1300

    Use bold rather than a different background to highlight current line number

--- color-theme-sanityinc-tomorrow.el
+++ color-theme-sanityinc-tomorrow.el
@@ -342,7 +342,7 @@ names to which it refers are bound."
       (fringe (:background ,low-contrast-bg :foreground ,comment))
       (linum (:background ,low-contrast-bg :foreground ,comment :italic nil :underline nil))
       (line-number (:background ,low-contrast-bg :foreground ,comment))
-      (line-number-current-line (:inherit highlight))
+      (line-number-current-line (:inherit line-number :foreground ,foreground :weight bold))
       (vertical-border (:foreground ,contrast-bg))
       (border (:background ,contrast-bg :foreground ,highlight))
       (border-glyph (nil))

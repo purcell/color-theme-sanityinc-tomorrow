e3e051f88734593d4b7b92f157e618ebfe63693b
Steve Purcell <steve@sanityinc.com>
2017-12-03 14:59:55 +1300

    Remove obsolete flymake-{warnline,errline} faces
    See #102

--- color-theme-sanityinc-tomorrow.el
+++ color-theme-sanityinc-tomorrow.el
@@ -247,8 +247,6 @@ names to which it refers are bound."
       (flycheck-color-mode-line-success-face (:foreground ,green))
 
       ;; Flymake
-      (flymake-warnline (:underline (:style wave :color ,orange) :background ,background))
-      (flymake-errline (:underline (:style wave :color ,red) :background ,background))
       (flymake-error (:underline (:style wave :color ,red)))
       (flymake-note (:underline (:style wave :color ,aqua)))
       (flymake-warning (:underline (:style wave :color ,orange)))

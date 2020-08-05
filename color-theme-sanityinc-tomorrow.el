ff26dea7ae0bb9be2fa5b395bc2a3826657d020a
Steve Purcell <steve@sanityinc.com>
2020-08-05 18:03:41 +1200

    Switch homepage URL to HTTPS

--- color-theme-sanityinc-tomorrow.el
+++ color-theme-sanityinc-tomorrow.el
@@ -4,7 +4,7 @@
 
 ;; Author: Steve Purcell <steve@sanityinc.com>
 ;; Keywords: faces themes
-;; Homepage: http://github.com/purcell/color-theme-sanityinc-tomorrow
+;; Homepage: https://github.com/purcell/color-theme-sanityinc-tomorrow
 ;; Version: 0
 
 ;; This program is free software; you can redistribute it and/or modify
